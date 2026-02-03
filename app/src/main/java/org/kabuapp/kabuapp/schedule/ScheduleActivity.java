3e94081b124ef92f419c4151a03dc1b4ebdad09a
les <leon.schmid@ivfp.de>
2026-02-03 14:23:03 +0100

    cleanup

--- app/src/main/java/org/kabuapp/kabuapp/schedule/ScheduleActivity.java
+++ app/src/main/java/org/kabuapp/kabuapp/schedule/ScheduleActivity.java
@@ -29,7 +29,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
